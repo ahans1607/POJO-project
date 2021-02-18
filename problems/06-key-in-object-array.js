a2baeb92861fee1c554fae2a6c238fedb0d44fe6
Ananya Hans <ananyahans@Ananyas-iMac.centurylink.net>
2021-02-17 22:15:07 -0600

    6

--- problems/06-key-in-object-array.js
+++ problems/06-key-in-object-array.js
@@ -20,8 +20,25 @@ keyInObjectArray(objArray, 'animal'); // => false
 ***********************************************************************/
 
 function keyInObjectArray(objArray, keyString) {
-  // your code here
+  for (let i = 0; i < objArray.length; i++){
+    let object = objArray[i]
+      if(Object.keys(object).includes(keyString)){
+        return true
+      }
+  }
+  return false
 }
 
+
+let objArray = [
+  { name: "Rupert" },
+  { age: 42 },
+  { planet: "Earth", system: "Milky Way" }
+];
+
+console.log(keyInObjectArray(objArray, 'planet')); // => true
+console.log(keyInObjectArray(objArray, 'age')); // => true
+console.log(keyInObjectArray(objArray, 'food')); // => false
+console.log(keyInObjectArray(objArray, 'animal')); // => false
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = keyInObjectArray;
