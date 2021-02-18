c340326e11d12f8524ca7c341f3ece19786d727b
Ananya Hans <ananyahans@Ananyas-iMac.centurylink.net>
2021-02-17 22:15:32 -0600

    07

--- problems/07-break-down-object.js
+++ problems/07-break-down-object.js
@@ -14,9 +14,15 @@ let object2 = {location: 'NY', borough: 'Brooklyn'};
 breakDownObj(object2); // => [ 'location', 'borough', 'NY', 'Brooklyn' ]
 ***********************************************************************/
 
-function breakDownObj(obj) {
-  // your code here
-}
+breakDownObj = (obj) => [...Object.keys(obj), ...Object.values(obj)]
+
+// Object.entries(obj).flat(Infinity)
+
+let object1 = {name: 'Rupert', age: 5, speak: 'Meow'};
+console.log(breakDownObj(object1)); // => [ 'name', 'age', 'speak', 'Rupert', 5, 'Meow' ]
+
+let object2 = {location: 'NY', borough: 'Brooklyn'};
+console.log(breakDownObj(object2)); // => [ 'location', 'borough', 'NY', 'Brooklyn' ]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = breakDownObj;
